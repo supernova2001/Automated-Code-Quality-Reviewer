f310ded6a5e1af18296d99ddff289640976ac6ac
supernova2001 <janakiramsharma18@gmail.com>
2025-06-05 01:29:21 -0400

    made change to the URL on frontend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API_URL = (process.env.REACT_APP_API_URL || "http://localhost:8000").replace(/\/$/, "");
 export default API_URL; 
